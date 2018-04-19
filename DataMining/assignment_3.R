772763fc58e2b2357ffd912e0f2984d8f2496a3c
Alex <alexaubrey@Alexs-MacBook-Pro.local>
2018-04-18 19:08:57 -0500

    new covariance function, need to improve

--- DataMining/assignment_3.R
+++ DataMining/assignment_3.R
@@ -5,28 +5,32 @@ ds <- ds[,c(1:10)]
 
 #1.Compute the multivariate mean vector
 #ds$multivariateMean <- rowMeans(ds)
-mu <- colMeans(ds)
-
-#z <- data.frame()
-z <- ds
-
-for (i in dim(ds)[1]) {   #rows
-  for (j in dim(ds)[2]) { #col
-    z[i,j] <- (ds[i,j] - mu[j])
-  }
-}
+mu <- colMeans(ds) #SHOULD THIS BE ROW MEANS?
 
 #2. Compute the sample covariance matrix as inner products between the columns of the centered data matrix, 
 #without using the covariance function in R. Compare and contrast your results with that obtained using 
 #the covariance function in R.
 
+dot_product <- function(x,y) {
+  
+  if (length(x) != length(y)) {
+    return("Vectors must be same size")
+  }
+  return(sum(x*y))
+}
+
 covariance_f <- function(dataset) {
   
-  #mu <- colMeans(dataset)
+  mu <- colMeans(dataset)
   
-  #tmp <- data.frame()
-  #for (i in names(dataset)) {
-  #  
-  #}
-  #return(colMeans(dataset))
+  Z <- data.frame()
+  
+  ### Center Matrix ###
+  for (i in 1:dim(ds)[1]) {   #rows
+    for (j in 1:dim(ds)[2]) { #col
+      Z[i,j] <- (ds[i,j] - mu[j])
+    }
+  }
+  print(dot_product(Z[,1],Z[,2]) / length(Z[,1]))
+  return(Z)
 }
