dc546539cf1941864cf3bb935efd310c5979c3a8
Alex <alexaubrey@Alexs-MacBook-Pro.local>
2018-06-04 06:45:05 -0500

    nn final added

--- PredAnalytics/NN/nn_hw.R
+++ PredAnalytics/NN/nn_hw.R
@@ -19,103 +19,99 @@ data[data$Cabin == "",]$Cabin <- "NULL"
 ## Clean Data ##
 
 #remove passengerID as this is a unique identifier
-#train$PassengerId <- NULL
-#test$PassengerId <- NULL
 data$PassengerId <- NULL
 
 ### get last name ###
 library(stringr)
 #grab everything up until the first comma.. should be last name.
-#train$LastName  <- str_extract(train$Name, "^[^,]+")
-#test$LastName  <- str_extract(test$Name, "^[^,]+")
 data$LastName  <- str_extract(data$Name, "^[^,]+")
 
+#Grab everything after the first comma up until the next space..
 tmp <- str_extract(data$Name, ", [^ ]+")
 data$Title <- str_extract(tmp, "[^ ]+$")
 rm(tmp)
 
-data <- cbind(data, model.matrix(~ Title - 1, data=data))
-data$Title <- NULL
+#Global list to keep track of all unique instances of value in the title column
+g_title <- list()
 
-#Onehot encode 
-#train <- cbind(train, model.matrix(~ LastName - 1, data=train))
-#test <- cbind(test, model.matrix(~ LastName - 1, data=test))
-data <- cbind(data, model.matrix(~ LastName - 1, data=data))
+convert_title_to_num <- function(dp) {
+  i <- length(g_title) + 1
+  if (dp %in% g_title) {
+    return(which(g_title == dp))
+  }
+  g_title[i] <<- dp
+  return(i)
+}
+data$Title <- sapply(data$Title, convert_title_to_num)
 
-#We can remove Full Name now.
-#train$Name <- NULL
-#test$Name <- NULL
+
+g_ln <- list()
+convert_ln_to_num <- function(dp) {
+  i <- length(g_ln) + 1
+  if (dp %in% g_ln) {
+    return(which(g_ln == dp))
+  }
+  g_ln[i] <<- dp
+  return(i)
+}
+data$LastName <- sapply(data$LastName, convert_ln_to_num)
+
+#We can remove the full name feature now.
 data$Name <- NULL
-data$LastName <- NULL
 
-
-#Convert Sex to nums
-#train$male <- ifelse(train$Sex == 'male', 1, 0)
-#train$female <- ifelse(train$Sex == 'female', 1, 0)
-#test$male <- ifelse(test$Sex == 'male', 1, 0)
-#test$female <- ifelse(test$Sex == 'female', 1, 0)
+#Expand the Sex col and remove.
 data$male <- ifelse(data$Sex == 'male', 1, 0)
 data$female <- ifelse(data$Sex == 'female', 1, 0)
-
-
-#We can now drop the Sex col.. 
-#train$Sex <- NULL
-#test$Sex <- NULL
 data$Sex <- NULL
 
-
-#Convert Embarked to nums
-#train$Embarked_C <- ifelse(train$Embarked == 'C', 1, 0)
-#train$Embarked_Q <- ifelse(train$Embarked == 'Q', 1, 0)
-#train$Embarked_S <- ifelse(train$Embarked == 'S', 1, 0)
-#test$Embarked_C <- ifelse(test$Embarked == 'C', 1, 0)
-#test$Embarked_Q <- ifelse(test$Embarked == 'Q', 1, 0)
-#test$Embarked_S <- ifelse(test$Embarked == 'S', 1, 0)
+#Expand the Embarked Column and remove.
 data$Embarked_C <- ifelse(data$Embarked == 'C', 1, 0)
 data$Embarked_Q <- ifelse(data$Embarked == 'Q', 1, 0)
 data$Embarked_S <- ifelse(data$Embarked == 'S', 1, 0)
-
-
-#We can now drop the Embarked col..
-#train$Embarked <- NULL
-#test$Embarked <- NULL
 data$Embarked <- NULL
 
-#Convert to factors
-#data$Pclass <- as.factor(data$Pclass)
-
-#Get meaning from ticket..
+#extract first part out of ticket
 data$ticket_title <- str_extract(data$Ticket, "^[^ ]+")
 data$ticket_title <- ifelse(grepl("^[0-9]+$", data$ticket_title), "all_nums", data$ticket_title)
-data <- cbind(data, model.matrix(~ ticket_title - 1, data=data))
+
+g_tt <- list()
+convert_tt_to_num <- function(dp) {
+  i <- length(g_tt) + 1
+  if (dp %in% g_tt) {
+    return(which(g_tt == dp))
+  }
+  g_tt[i] <<- dp
+  return(i)
+}
+#Get all unique instances from ticket_title col and remove ticket.
+data$ticket_title <- sapply(data$ticket_title, convert_tt_to_num)
 data$Ticket <- NULL
-data$ticket_title <- NULL
 
-#data[data$Cabin == "",]$Cabin <- "NULL"
-data <- cbind(data, model.matrix(~ Cabin - 1, data=data))
-data$Cabin <- NULL
+#Get all unique instances from Cabin.
+g_c <- list()
+convert_c_to_num <- function(dp) {
+  i <- length(g_c) + 1
+  if (dp %in% g_c) {
+    return(which(g_c == dp))
+  }
+  g_c[i] <<- dp
+  return(i)
+}
+data$Cabin <- sapply(data$Cabin, convert_c_to_num)
 
-#data$Parch <- as.numeric(data$Parch)
-#data$Pclass <- as.numeric(data$Pclass)
-#data$SibSp <- as.numeric(data$SibSp)
-#data$Age <- as.numeric(data$Age)
-#data$Fare <- as.numeric(data$Fare)
+#Scale numeric columns
+data[,c("Age","Fare")] <- scale(data[,c("Age","Fare")], center=TRUE, scale=TRUE)
 
-#data <- apply(data, 2, as.numeric)
-
-#Get train/test data back..
+#Get train/test data back after preprocessing..
 train <- data[data$type == 'train',]
-train$type <- NULL
-test <- data[data$type == 'test',]
-test$Survived <- NULL #Remove col we added so we could smoothly combine the df earlier..
-test$type <- NULL
+train$type <- NULL #remove type col that we created to keep track of train/test original
 
-#train[,c(2:1142)] <- as.data.frame(sapply(train[,c(2:1142)], as.numeric))
-train <- apply(as.data.frame(train), 2, as.numeric)
-train <- as.data.frame(train)
+test <- data[data$type == 'test',]
+test$type <- NULL #remove type col that we created to keep track of train/test original
 
 #Convert target var to factor
 train$Survived <- as.factor(train$Survived)
+test$Survived <- as.factor(test$Survived)
 
 n <- dim(train)[1]
 
@@ -124,10 +120,10 @@ t2 <- setdiff(1:n, t1)
 
 train_train <- train[t1, ]
 train_train <- as.data.frame(train_train)
-train_train$Survived <- as.factor(train_train$Survived)
+#train_train$Survived <- as.factor(train_train$Survived)
 train_test <- subset(train[t2,], select=- Survived)
 
-grid <- expand.grid(size=c(7,8,9), decay=c(0.01,0.1))
+grid <- expand.grid(size=c(6,7,8,9,10,11,12,13,14,15), decay=c(0,0.01,0.1,1))
 nfit <- train(Survived ~ ., data=train_train, 
               method="nnet", 
               tuneGrid=grid, 
@@ -137,5 +133,10 @@ nfit <- train(Survived ~ ., data=train_train,
               MaxNWts = 20000)
 
 pred <- predict(nfit, train_test, type="raw")
-confusionMatrix(pred)
+confusionMatrix(pred, train[t2,]$Survived)
 
+
+pred <- predict(nfit, test, type="raw")
+
+print("Number of survival..")
+table(pred)
