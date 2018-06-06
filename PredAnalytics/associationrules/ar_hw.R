58cd9943b22bb1bdad2b95ec8e303c2315da7bf8
Alex <alexaubrey@Alexs-MacBook-Pro.local>
2018-06-06 07:06:21 -0500

    finished association rules

--- PredAnalytics/associationrules/ar_hw.R
+++ PredAnalytics/associationrules/ar_hw.R
@@ -19,6 +19,51 @@ pre_data <- pre_data[,c("ChildBks", "YouthBks", "CookBks", "DoItYBks", "RefBks",
 
 write.table(pre_data, file = "maincharlesbook_clean.csv", sep=",", row.names = FALSE, col.names=FALSE)
 
+### Clean the data with our python script ###
+system("python strip_data2.py")
+
+### Contents of python script ###
+# import os
+# import re
+# 
+# ### Take out all instances of null strings values and shift them to the left after we transformed the data in R ###
+# outfile = open("maincharlesbook_cleaned.csv","w")
+# 
+# for line in open("maincharlesbook_clean.csv"):
+#   #Replace all instances that exist in the line
+#   for item in range(0, line.count('NA,')):
+#     line = line.replace('NA,', '')
+# 
+# #Take care of potential NA on the end
+# line = line.replace(',NA', '')
+# 
+# 
+# outfile.write(line)
+# 
+# outfile.close()
+# 
+# ### Take out all the  null values and shift them to the left after we took out the null strings ###
+# outfile = open("maincharlesbook_clean_final.csv", 'w')
+# 
+# for line in open("maincharlesbook_cleaned.csv"):
+#   #Replace all instances that exist in the line   
+#   for item in range(0, line.count(',,')):
+#     line = line.replace(',,', ',')
+# 
+# #Replace comma at the end of the line
+# line = re.sub('\,$', '',line)
+# 
+# #Don't want anything where no one bought any of our books
+# if re.match('^NA$', line):
+#   pass
+# else:
+#   outfile.write(line)
+# 
+# outfile.close()
+# 
+# #Don't need the tmp file anymore..
+# os.system("rm maincharlesbook_cleaned.csv")
+
 data = read.transactions("maincharlesbook_clean_final.csv", format="basket", sep=",")
 
 rules <- apriori(data=data, parameter = list(supp = 0.1, conf=0.8, minlen=2),
--- PredAnalytics/associationrules/strip_data2.py
+++ PredAnalytics/associationrules/strip_data2.py
@@ -42,3 +42,49 @@ outfile.close()
 
 #Don't need the tmp file anymore..
 os.system("rm maincharlesbook_cleaned.csv")
+
+#print out for our homework submission
+print("""import os
+import re
+
+### Take out all instances of null strings values and shift them to the left after we transformed the data in R ###
+outfile = open("maincharlesbook_cleaned.csv","w")
+
+for line in open("maincharlesbook_clean.csv"):
+        #Replace all instances that exist in the line
+        #for item in range(0, line.count(',NA')):
+        for item in range(0, line.count('NA,')):
+                #line = line.replace(',NA', ',')
+                line = line.replace('NA,', '')
+
+        #Take care of potential NA on the end
+        line = line.replace(',NA', '')
+
+        #Replace first col of NAs as well
+        #line = re.sub('^NA\,', '',line)
+
+        outfile.write(line)
+
+outfile.close()
+
+### Take out all the  null values and shift them to the left after we took out the null strings ###
+outfile = open("maincharlesbook_clean_final.csv", 'w')
+
+for line in open("maincharlesbook_cleaned.csv"):
+        #Replace all instances that exist in the line   
+        for item in range(0, line.count(',,')):
+                line = line.replace(',,', ',')
+
+        #Replace comma at the end of the line
+        line = re.sub('\,$', '',line)
+
+        #Don't want anything where no one bought any of our books
+        if re.match('^NA$', line):
+                pass
+        else:
+                outfile.write(line)
+
+outfile.close()
+
+#Don't need the tmp file anymore..
+os.system("rm maincharlesbook_cleaned.csv")""")
